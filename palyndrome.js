df0e828be5f8f095bac78599f46a841e1242760d
Jordan Tanaliga <43674051+tanaliga100@users.noreply.github.com>
2022-07-22 15:47:02 +0800

    palyndrome

---
+++ palyndrome.js
@@ -0,0 +1,21 @@
+export default function Palyn() {
+  function checkPalyndrome(args) {
+    const splitter = args.split("");
+    const handleReverse = splitter.reverse();
+    const toCheck = handleReverse.join("");
+
+    if (args === toCheck) {
+      console.log("Correct  ! that is a palyndrome");
+      return;
+    } else {
+      console.log("Wrong ! that is not palyndrome");
+      return;
+    }
+  }
+  const args = prompt("Enter a string: ");
+
+  checkPalyndrome(args);
+
+  return <div>PalynDrome</div>;
+}
+
